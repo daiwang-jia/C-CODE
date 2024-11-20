ddf1d84752cfa9cbe78224fe97462f5e94c2ec90
giteejdw2005 <3472432052@qq.com>
2024-11-20 23:30:38 +0800

    1

---
+++ 11-20/11-20/11-20.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+//int my_strlen(char* str)
+//{
+//	int count = 0;
+//	while (*str != '\0')
+//	{
+//		count++;
+//		str++;
+//	}
+//	return count;
+//}
+//µİ¹éÇó½â
+int my_strlen(char* str)
+{
+	if (*str != '\0')
+	{
+		return 1 + my_strlen(str + 1);
+	}
+	else
+	{
+		return 0;
+	}
+}
+int main()
+{
+	char arr[] = "abc";
+	int len = my_strlen(arr);
+	printf("%d\n", len);
+	return 0;
+}
