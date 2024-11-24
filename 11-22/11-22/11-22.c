7640bafd960e8efda1361c589e74959ff22e27d1
giteejdw2005 <3472432052@qq.com>
2024-11-24 14:35:39 +0800

    1

---
+++ 11-22/11-22/11-22.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+//int fac(int a)
+//{
+//	if (a <= 1)
+//	{
+//		return 1;
+//	}
+//	else
+//	{
+//		return a * fac(a-1);
+//	}
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf_s("%d", &n);
+//	int ret = fac(n);
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+//ì³²¨ÄÇÆõÊıÁĞ
+//int fib(a)
+//{
+//	if (a<= 2)
+//	{
+//		return 1;
+//	}
+//	else
+//	{
+//		return fib(a - 1) +fib(a - 2);
+//	}
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf_s("%d", &n);
+//	int ret = fib(n);
+//	printf("%d", ret);
+//	return 0;
+//
+//}
+//int fib(int x)
+//{
+//	int a = 1;
+//	int b = 1;
+//	int c = 0;
+//	while (x >= 3)
+//	{
+//		c = a + b;
+//		a = b;
+//		b = c;
+//		x--;
+//	}
+//	return c;
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf_s("%d", &n);
+//	int ret = fib(n);
+//	printf("%d", ret);
+//	return 0;
+//}
+
+ 
