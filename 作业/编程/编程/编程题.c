91da73d7e7558b12cd782db33ac3620c03bdb283
giteejdw2005 <3472432052@qq.com>
2025-01-25 15:12:48 +0800

    1

--- 作业/编程/编程/编程题.c
+++ 作业/编程/编程/编程题.c
@@ -1,18 +1,39 @@
 
-//1 不使用数组下标，打印数组内容
+////1 不使用数组下标，打印数组内容
+//#include <stdio.h>
+//void print(int* p, int sz)
+//{
+//	for (int i = 0; i < sz; i++)
+//	{
+//		printf("%d ", *(p + i));
+//	}
+//	printf("\n");
+//}
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	print(arr, sz);
+//	return 0;
+//}
+
+//字符串逆序
 #include <stdio.h>
-void print(int* p, int sz)
-{
-	for (int i = 0; i < sz; i++)
-	{
-		printf("%d ", *(p + i));
-	}
-	printf("\n");
-}
+#include <string.h>
 int main()
 {
-	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
-	int sz = sizeof(arr) / sizeof(arr[0]);
-	print(arr, sz);
+	char arr[10001] = { 0 };
+	gets(arr);
+	int left = 0;
+	int right = strlen(arr) - 1;
+	while (left < right)
+	{
+		char tmp = arr[left];
+		arr[left] = arr[right];
+		arr[right] = tmp;
+		left++;
+		right--;
+	}
+	printf("%s\n", arr);
 	return 0;
 }
