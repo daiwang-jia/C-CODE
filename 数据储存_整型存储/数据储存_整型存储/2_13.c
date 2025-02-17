2ec6de4d3203bc9087c9055f96ff505bc9c3f77a
giteejdw2005 <3472432052@qq.com>
2025-02-17 20:53:15 +0800

    1

--- 数据储存_整型存储/数据储存_整型存储/2_13.c
+++ 数据储存_整型存储/数据储存_整型存储/2_13.c
@@ -31,15 +31,84 @@
 //}
 
 
+//#include <stdio.h>
+//#include <Windows.h>
+//int main()
+//{
+//	unsigned int i;
+//	for (i = 9; i >= 0; i--)
+//	{
+//		printf("%u\n", i);
+//		Sleep(1000);
+//	}
+//	return 0;
+//}
 #include <stdio.h>
-#include <Windows.h>
+void menu()
+{
+    printf("****************************\n");
+    printf("*******1.add    2.sub*******\n");
+    printf("*******3.mul    4.div*******\n");
+    printf("*******0.exit    ***********\n");
+    printf("****************************\n");
+}
+
+int Add(int x, int y)
+{
+    return x + y;
+}
+
+int Sub(int x, int y)
+{
+    return x - y;
+}
+
+int Mul(int x, int y)
+{
+    return x * y;
+}
+int Div(int x, int y)
+{
+    return x / y;
+}
 int main()
 {
-	unsigned int i;
-	for (i = 9; i >= 0; i--)
-	{
-		printf("%u\n", i);
-		Sleep(1000);
-	}
-	return 0;
+    int input = 0;
+    int x = 0;
+    int y = 0;
+    int ret = 0;
+    do
+    {
+        menu();
+        printf("请选择:>");
+        scanf_s("%d", &input);
+        printf("请输入两个操作数:>");
+        scanf_s("%d %d", &x, &y);
+        switch (input)
+        {
+        case 1:
+            ret = Add(x, y);
+            printf("%d\n", ret);
+            break;
+        case 2:
+            ret = Sub(x, y);
+            printf("%d\n", ret);
+            break;
+        case 3:
+            ret = Mul(x, y);
+            printf("%d\n", ret);
+            break;
+        case 4:
+            ret = Div(x, y);
+            printf("%d\n", ret);
+            break;
+        case 0:
+            printf("退出计算器\n");
+            break;
+        default:
+            printf("选择错误\n");
+            break;
+        }
+    } while (input);
+    return 0;
 }
